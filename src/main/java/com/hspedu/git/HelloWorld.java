632dfee6cc84cc2fb7fe2a2c24f5e9d887332993
sunhaotian <858941123@qq.com>
2022-09-03 19:26:36 +0800

    在idea修改代码，push给github

--- src/main/java/com/hspedu/git/HelloWorld.java
+++ src/main/java/com/hspedu/git/HelloWorld.java
@@ -3,5 +3,7 @@ package com.hspedu.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+
+        System.out.println("在idea修改代码，push给github");
     }
 }
